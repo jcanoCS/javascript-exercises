65e02907cf4924cf1860f5c00a637e711c9999fe
Jose Cano <cano.5@wright.edu>
2023-09-13 00:04:34 -0400

    Completed the 2nd javascript exercise and test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, numRepeat) {
+    if (numRepeat < 0){
+        return "ERROR";
+    }
+    
+    repeatedWord = "";
+
+    for (let i = numRepeat; i > 0; i--) {
+        repeatedWord += word;
+    }
+
+    return repeatedWord;
 
 };
 
