c3da712e82098878c76878a91bfcb573642c6b81
Jose Cano <cano.5@wright.edu>
2023-09-14 14:20:39 -0400

    Verified exercise 06 tests pass and comments
    The .spec.js file was modified to enable all tests
    for the leapYear function. All tests were verified
    to pass.
    Comments were added to describe the logic inside the
    function.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
 const leapYears = function(year) {
+    // Is only a leap year if one of two conditions is true
+    // 1) Year divisible by 4 but not 25
+    // or
+    // 2) Year divisible by 400
     return ((year % 4 === 0) && (year %  25 !== 0)) || (year % 400 == 0);
 };
 
