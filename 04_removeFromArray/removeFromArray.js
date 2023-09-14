1901ae963b5b1616e79e9447e0df8261c8499601
Jose Cano <cano.5@wright.edu>
2023-09-14 02:36:24 -0400

    Exercise 04 passed test #3
    The function was modified to be able to ignore non present
    values by checking if the value is included in the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,11 +2,18 @@ const removeFromArray = function() {
     // array to reduce from will be first function parameter
     const arrayToReduce = arguments[0];
     const SINGLE_ELEMENT = 1;
+    let elementToRemove;
     let indexToRemove;
 
     // loop to remove all remaining parameters from array
     for(let i = 1; i < arguments.length; i++) {
-        indexToRemove = arrayToReduce.indexOf(arguments[i]);
+        elementToRemove = arguments[i];
+        // if the element is not in array, continue next iteration
+        if (!arrayToReduce.includes(elementToRemove)) {
+            continue;
+        }
+        // otherwise, remove
+        indexToRemove = arrayToReduce.indexOf(elementToRemove);
         arrayToReduce.splice(indexToRemove, SINGLE_ELEMENT);
     }
 
