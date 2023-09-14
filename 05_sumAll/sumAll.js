2cdb574417d36948e5a58d0784839eaf1f67f601
Jose Cano <cano.5@wright.edu>
2023-09-14 02:50:43 -0400

    Wrote function to pass exercise 05 test #1
    Wrote code inside the function to compute the sum between
    the ranges of given numbers. Also wrote parameters to the
    function to be able to identify the ranges.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let small = Math.min(num1, num2);
+    let large = Math.max(num1, num2);
+    let currentNumber = small;
+    let currentSum = 0;
 
+    while(currentNumber <= large) {
+        currentSum += currentNumber;
+        currentNumber++;
+    }
+
+    return currentSum;
 };
 
 // Do not edit below this line
