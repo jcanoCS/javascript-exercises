993cdaf4bd5859caec3f667ea519b3da4255aae5
Jose Cano <cano.5@wright.edu>
2023-09-14 03:00:22 -0400

    Function modification pass exercise 05 test #5
    A check was added to make sure both function parameters
    are numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(num1, num2) {
-    if (num1 < 0 || num2 < 0) {
+    if (num1 < 0 || num2 < 0 || !(typeof num1 === 'number') || +
+    !(typeof num2 === 'number')) {
         return "ERROR";
     }
     let small = Math.min(num1, num2);
