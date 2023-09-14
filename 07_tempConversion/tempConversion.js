ca88ea125526f07affdd6f48dd7c1a03fdacc5e2
Jose Cano <cano.5@wright.edu>
2023-09-14 14:41:34 -0400

    Finished second function, tests, and comments
    The convertToFahrenheit() functon was completed and verified to pass
    all tests.
    Comments were added to both functions for readability.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
 const convertToCelsius = function(fahrenheitDegrees) {
+  // calculate conversion
   let celsiusDegrees = (fahrenheitDegrees - 32.0) * (5.0/9.0);
+  // round to a single decimal place
   return Number(celsiusDegrees.toFixed(1));
 };
 
 
 const convertToFahrenheit = function(celsiusDegrees) {
+  // calculate conversion
+  let fahrenheitDegrees = (celsiusDegrees * (9.0/5.0)) + 32.0;
+  // round to a single decimal place
+  return Number(fahrenheitDegrees.toFixed(1));
 };
 
 // Do not edit below this line
