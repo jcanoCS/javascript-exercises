a0bce4991d6c4972e98428f2a3ec2fbdfe5e973d
Jose Cano <cano.5@wright.edu>
2023-09-14 14:34:15 -0400

    Modified function and passed tests #2 and #3
    The convertToCelsius() function was modified to round to a single
    decimal place. This passed tests #2 and #3.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,6 @@
 const convertToCelsius = function(fahrenheitDegrees) {
-  return (fahrenheitDegrees - 32.0) * (5.0/9.0);
+  celsiusDegrees = (fahrenheitDegrees - 32.0) * (5.0/9.0);
+  return Number(celsiusDegrees.toFixed(1));
 };
 
 const convertToFahrenheit = function() {
