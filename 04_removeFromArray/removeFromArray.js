78cd847690c6e15c3c12ab1eb425fe00f8752a4e
Jose Cano <cano.5@wright.edu>
2023-09-14 02:32:00 -0400

    Exercise 04 passed test 2
    The function was modified to handle a variable number of
    arguments that act as elements to remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,16 @@
-const removeFromArray = function(arrayToRemoveFrom, elementToRemove) {
-    let indexToRemove = arrayToRemoveFrom.indexOf(elementToRemove);
-    arrayToRemoveFrom.splice(indexToRemove, 1);
-    return arrayToRemoveFrom;
+const removeFromArray = function() {
+    // array to reduce from will be first function parameter
+    const arrayToReduce = arguments[0];
+    const SINGLE_ELEMENT = 1;
+    let indexToRemove;
+
+    // loop to remove all remaining parameters from array
+    for(let i = 1; i < arguments.length; i++) {
+        indexToRemove = arrayToReduce.indexOf(arguments[i]);
+        arrayToReduce.splice(indexToRemove, SINGLE_ELEMENT);
+    }
+
+    return arrayToReduce;
 }
 
 // Do not edit below this line
