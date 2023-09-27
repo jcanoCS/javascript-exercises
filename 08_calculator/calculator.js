059515342c2879dd4c23c6891b1cc470b333c52a
Jose Cano <cano.5@wright.edu>
2023-09-26 22:11:08 -0400

    Completed calculator exercise 06

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  arr.forEach((val) => {total += val; });
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let total;
+  arr.forEach((val) => {
+    if(total){
+      total *= val;
+    }
+    else {
+      total = val;
+    } 
+  });
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  while(num > 0) {
+    total *= num;
+    num -= 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
